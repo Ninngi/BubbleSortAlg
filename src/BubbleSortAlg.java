7864341805dc621731e07f8e85a68f306eb845ad
Patryk Pawlak <thepatrvq@gmail.com>
2023-02-17 16:36:54 +0100

    fix a problem with the sorting algorrithm

--- src/BubbleSortAlg.java
+++ src/BubbleSortAlg.java
@@ -1,16 +1,13 @@
 
 public class BubbleSortAlg {
         public void sort(int[] array) {
-            boolean isSorted;
+            
             for (var i = 0; i < array.length; i++) {
-                isSorted = true;
                 for (var j = 1; j < array.length - i; j++)
                     if (array[j] < array[j - 1]) {
                         swap(array, j, j - 1);
-                        isSorted = false;
                     }
                 if (isSorted);
-                return;
             }
         }
 
